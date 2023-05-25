0847341f775c93b9b743bd000d10dab89114f5df
kdeveloper9588 <kjlcodewww@gmail.com>
2023-05-25 23:15:54 +0900

    FEAT:충전, 결제, 조회 API

--- index.js
+++ index.js
@@ -29,6 +29,46 @@ connection.connect((err) => {
 app.use('/', require('./routes/pages'))
 app.use('/auth', require('./routes/auth'))
 
+app.post("/api/pay", (req, res) => {
+    const { minusPoint, code_number } = req.body;
+    console.log(minusPoint);
+    const sql = "update users set point = point - ? where code_number = ?";
+    connection.query(sql, [minusPoint, code_number], (err, result) => {
+      if (err) {
+        throw err;
+      }
+      console.log(result);
+      res.send("point save to database" + result);
+    });
+});
+
+app.post("/api/charge", (req, res) => {
+    const { plusPoint, code_number } = req.body;
+    console.log(plusPoint);
+    const sql = "update users set point = point + ? where code_number = ?";
+    connection.query(sql, [plusPoint, code_number], (err, result) => {
+        if (err) {
+        throw err;
+        }
+        console.log(result);
+        res.send("point save to database" + result);
+    });
+});
+
+app.get("/api/check", (req, res) => {
+    const { email, password } = req.body;
+    console.log(email, password);
+    const sql =
+        "select student_name, point from users WHERE email = ? and password = ?";
+    connection.query(sql, [email, password], (err, result) => {
+        if (err) {
+        throw err;
+        }
+        console.log(result);
+        res.send(result);
+    });
+});
+
 app.listen(port, (req, res) => {
     console.log(`WEB Server is running on port ${port}`)
 });
