a50dfd18411dd3655160d53c6234eebb4c9528c2
hyeonjoonpark <pjjoon1379@gmail.com>
2023-05-27 23:42:57 +0900

    FEAT:로그인,회원가입 구현

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mysql = require('mysql2')
 const path = require('path')
 const db = require('./config/db')
 const cors = require("cors");
+const bcrypt = require('bcrypt');
 
 const port = 9000;
 
@@ -26,8 +27,78 @@ connection.connect((err) => {
     }
 })
 
-app.use('/', require('./routes/pages'))
-app.use('/auth', require('./routes/auth'))
+app.post('/api/signup', async (req, res) => {
+    const { student_name, email, password } = req.body;
+    console.log(req.body);
+
+    try {
+        const [results] = await connection.promise().query(
+            'SELECT email FROM users WHERE email = ?', email
+        );
+
+        if (results.length > 0) {
+            console.log(results);
+
+            return res.status(400).json({
+                error: 'Email already exists'
+            })
+        }
+
+        const salt = await bcrypt.genSalt(Number(8));
+        const hashedPassword = await bcrypt.hash(String(password), salt);
+        console.log(hashedPassword);
+
+        const register_values = [student_name, email, hashedPassword];
+
+        const re = /\S+@bssm.hs.kr/;
+        const email_test_result = re.test(email);
+        if (email_test_result === false) {
+            return res.status(400).json({ error: 'Invalid email format' });
+        }
+
+        connection.query(
+            'INSERT INTO users(student_name, email, password) VALUES (?, ?, ?)', register_values
+        );
+
+        console.log('User registered successfully');
+        return res.status(200).json({ message: 'User registered successfully' });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.post('/api/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const [results] = await connection.promise().query(
+            'SELECT * FROM users WHERE email = ?',
+            email
+        );
+
+        if (results.length === 0) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+
+        const user = results[0];
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+
+        // 로그인 성공 처리
+        console.log("로그인 성공")
+        return res.status(200).json({ message: 'Login successful' });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+
 
 app.listen(port, (req, res) => {
     console.log(`WEB Server is running on port ${port}`)
--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express') // NodeJS 웹 프레임워크
-const loginController = require('../controllers/login');
-const registerController = require('../controllers/register');
-const router = express.Router();
-
-router.post('/register', registerController.register);
-router.post('/login', loginController.login);
-
-module.exports = router;
