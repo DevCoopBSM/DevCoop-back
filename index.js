d5e197394f970d03b415f513dec9dd5b889f80a2
KB <tfm0405@gmail.com>
2023-06-27 13:26:12 +0000

    FEAT:관리자 로그인 API

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const {connection, pool} = require('./utils/query')
 const app = express();
 const cors = require('cors');
 // const {verifyToken} = require('./utils/token')
+
+
 app.use(express.json());
 
 const port = 6002
@@ -13,6 +15,7 @@ const signupRouter = require("./routes/signup");
 const loginRouter = require("./routes/login");
 const meRouter = require("./routes/me")
 const stinfoRouter = require("./routes/stinfo")
+const adminloginRouter = require("./routes/adminlogin");
 
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
@@ -26,34 +29,100 @@ connection.connect((err) => {
     }
 })
 
-
-
-
-
-app.post("/api/charge", (req, res) => {
-    const { plusPoint, code_number } = req.body;
-    const sql = "update users set point = point + ? where code_number = ?";
-    connection.query(sql, [plusPoint, code_number], (err, result) => {
-        try {
-            return res.status(200).json({ message: "충전을 성공하였습니다" });
-        } catch (err) {
-            return res.status(500).json({ error: "포인트 충전 실패" });
-        }
-    });
-});
+// app.post("/api/charge", (req, res) => {
+//     const { plusPoint, code_number } = req.body;
+//     const sql = "update users set point = point + ? where code_number = ?";
+//     connection.query(sql, [plusPoint, code_number], (err, result) => {
+//         try {
+//             return res.status(200).json({ message: "충전을 성공하였습니다" });
+//         } catch (err) {
+//             return res.status(500).json({ error: "포인트 충전 실패" });
+//         }
+//     });
+// });
 
 app.get("/api/check", (req, res) => {
-    const email = req.body;
-    const sql =
-        "select point from users WHERE email = ?";
-    connection.query(sql, [email], (err, result) => {
+    const {code_number} = req.body;
+    const sql ="select point, student_number from users WHERE code_number = ?";
+    connection.query(sql, [code_number] , (err, result) => {
         try {
-            return res.status(200).json({ message: "포인트 조회 성공" });
+            return res.status(200).json({
+              message: "success",
+              users: result
+            });
         } catch (err) {
-            return res.status(500).json({ error: "포인트 조회 실패" });
+            return res.status(500).json({ 
+              message: "success fail",
+              error: err
+             });
         }
     });
 });
+
+app.post("/api/charge", (req, res) => {
+  const {plusPoint, code_number } = req.body;
+  console.log(plusPoint, code_number);
+  const sql1 = "select student_number, point from users where code_number = ?"; //5000
+  const sql2 = "update users set point = point + ? where code_number = ?"; //-1000
+  const sql3 = "select point from users where code_number = ?"; //4000 //4000 4000 4000 -2000 2000??
+  connection.query(sql1, [code_number], (err, result1) => {
+    if (err) {
+      throw err;
+    }
+    const value1 = result1[Object.keys(result1)[0]];  
+    const now_point = value1.point;
+    const response1 = {
+      학번: value1.student_number,
+      "원래 금액": value1.point,
+      "충전 금액": plusPoint,
+    };
+    connection.query(sql2, [plusPoint, code_number], (err, result2) => {
+      if (err) {
+        throw err;
+      }
+      connection.query(sql3, [code_number], (err, result3) => {
+        if (err) {
+          throw err;
+        }
+        const value2 = result3[Object.keys(result3)[0]];
+        const response2 = {
+          "최종 잔액": value2.point,
+          message: "성공",
+        };  
+        const newresponse = { ...response1, ...response2 };
+        console.log(newresponse);
+        res.status(200).send(newresponse);
+      });
+    });
+  });
+});
+
+
+// app.post("/api/charge", async(req, res) => {
+//     const { code_number, plusPoint } = req.body;
+//     console.log(plusPoint);
+//     try {
+//         await connection.beginTransaction();
+//         const sql1 = "select student_number, point from users where code_number = ?";
+//         const sql2 = "insert into log values(?, ?, ?, ?)";
+//         const sql3 = "update users set point = point + ? where code_number = ?";
+//         const sql4 = "select point from users where code_number = ?";
+
+//         const select = await connection.query(sql1, [code_number]);
+//         const upd = await connection.query(sql3, [plusPoint, code_number]);
+//         const reselect = await connection.query(sql4, [code_number]);
+
+//         await connection.commit();
+//         const result = {...select, ...reselect};
+//         console.log(result);
+//         return res.status(200).send(result);
+//     } catch (error) {
+//       console.log(err)
+//       await connection.rollback() // 롤백
+//       return res.status(500).json(err)
+//     }finally {
+//       connection.release();}
+// });
 
         // app.get("/api/studentinfo", (req, res) => {
         //     const verifyedToken = verifyToken(req.header('Authorization'))
@@ -121,11 +190,11 @@ app.post("/api/pays", (req, res) => {
     });
   });
 
-
 app.use("/api/signup", signupRouter);
 app.use("/api/login", loginRouter);
 app.use("/api/me", meRouter);
 app.use("/api/studentinfo", stinfoRouter);
+app.use("/api/adminlogin", adminloginRouter);
 
 // CORS 하용 설정하기.
 app.use((req, res, next) => {
@@ -155,18 +224,18 @@ app.post("/api/pay", (req, res) => {
     });
 });
 
-app.post("/api/charge", (req, res) => {
-    const { plusPoint, code_number } = req.body;
-    console.log(plusPoint);
-    const sql = "update users set point = point + ? where code_number = ?";
-    connection.query(sql, [plusPoint, code_number], (err, result) => {
-        if (err) {
-        throw err;
-        }
-        console.log(result);
-        res.send("point save to database" + result);
-    });
-});
+// app.post("/api/charge", (req, res) => {
+//     const { plusPoint, code_number } = req.body;
+//     console.log(plusPoint);
+//     const sql = "update users set point = point + ? where code_number = ?";
+//     connection.query(sql, [plusPoint, code_number], (err, result) => {
+//         if (err) {
+//         throw err;
+//         }
+//         console.log(result);
+//         res.send("point save to database" + result);
+//     });
+// });
 
 app.get("/api/check", (req, res) => {
     const { email, password } = req.body;
---
+++ routes/adminlogin.js
@@ -0,0 +1,51 @@
+const express = require('express');// NodeJS 웹 프레임워크
+const bcrypt = require('bcrypt');
+const router = express.Router();
+const {connection} = require("../utils/query")
+const { genToken, updateRefToken } = require('../utils/token');
+router.use(express.json());
+
+
+    
+router.post('/', async (req, res) => {
+    const { email, password } = req.body;
+    console.log(email)
+
+    try {
+        const query = 'SELECT * FROM users WHERE email = ?';
+        // console.log(connection)
+        const [results] = await connection.promise().query(query, email);
+        console.log(results);
+        if (results.length === 0) {
+            return res.status(401).json({ error: '이메일이 잘못되었습니다' });
+        }
+
+        const user = results[0];
+        // const isPasswordValid = (password == user.password)
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        // console.log(isPasswordValid);
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: '비밀번호가 잘못되었습니다' });
+        }
+        if(user.is_admin != 1){
+            return res.status(401).json({error:'관리자가 아닙니다'});
+        }
+        const accessToken = await genToken(email, user.student_name, "1h");
+        const refreshToken = await genToken(email, user.student_name, "14d");
+        // updateToken("acc_token", email, accessToken);
+        updateRefToken(email, refreshToken);
+        // console.log([accessToken, refreshToken]);
+        // console.log("로그인 성공");
+        return res.status(200).json({
+            message: '로그인이 성공적으로 되었습니다',
+            accToken: accessToken,
+            refToken: refreshToken,
+            name: user.student_name,
+            point: user.point
+        });
+    } catch (err) {
+        console.error(err)
+        return res.status(500).json({ error: '내부 서버 오류가 발생하였습니다' });
+    }
+});
+module.exports = router;
