644b3958ae1a9b9deb1d9386161e51a347ff8fe6
KimKyuBong <tfm0405@gmail.com>
2023-09-14 14:23:15 +0000

    이제 도커끼리의 소통 url은 도커명으로..

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-	  host: "127.0.0.1",
-	  port: 6006,
+	  host: "DevCoop_MySql",
+	  port: 3306,
 	  user: process.env.DB_USER_NAME,
 	  password: process.env.DB_PASSWORD,
 	  database: process.env.DB_NAME,
