9bb33b5dd3bd3aa712a22a9d7417b1dcfac1e372
KimKyuBong <tfm0405@gmail.com>
2023-11-17 07:48:50 +0000

    g

--- models/index.js
+++ models/index.js
@@ -1,25 +1,25 @@
-"use strict";
+'use strict';
 
-const fs = require("fs");
-const path = require("path");
-const Sequelize = require("sequelize");
-const process = require("process");
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
-const config = require("../config/config")[env];
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
 const db = {};
 
 function toPascalCase(str) {
   return (
     str
       // 먼저 모든 언더스코어(_)를 공백으로 치환합니다.
-      .replace(/_/g, " ")
+      .replace(/_/g, ' ')
       // 단어의 첫 글자만 대문자로 변환합니다.
       .replace(/\w\S*/g, (txt) => {
         return txt.charAt(0).toUpperCase() + txt.substr(1);
       })
       // 공백을 제거합니다.
-      .replace(/\s/g, "")
+      .replace(/\s/g, '')
   );
 }
 
@@ -31,25 +31,25 @@ if (config.use_env_variable) {
     config.database,
     config.username,
     config.password,
-    config,
+    config
   );
 }
 
 fs.readdirSync(__dirname)
   .filter((file) => {
     return (
-      file.indexOf(".") !== 0 &&
+      file.indexOf('.') !== 0 &&
       file !== basename &&
-      file.slice(-3) === ".js" &&
-      file.indexOf(".test.js") === -1
+      file.slice(-3) === '.js' &&
+      file.indexOf('.test.js') === -1
     );
   })
   .forEach((file) => {
     const model = require(path.join(__dirname, file))(
       sequelize,
-      Sequelize.DataTypes,
+      Sequelize.DataTypes
     );
-    const modelName = toPascalCase(file.replace(".js", ""));
+    const modelName = toPascalCase(file.replace('.js', ''));
     db[modelName] = model;
   });
 
@@ -61,39 +61,39 @@ Object.keys(db).forEach((modelName) => {
 
 // Items 모델에 스냅샷 관계 설정
 db.Items.hasMany(db.InventorySnapshots, {
-  foreignKey: "itemId", // InventorySnapshots 모델의 컬럼 이름
-  as: "snapshots",
+  foreignKey: 'itemId', // InventorySnapshots 모델의 컬럼 이름
+  as: 'snapshots',
 });
 
 db.Items.hasMany(db.Receipt, {
-  foreignKey: "item_id", // Receipt 모델의 아이템 관련 컬럼 이름
-  as: "receipts",
+  foreignKey: 'item_id', // Receipt 모델의 아이템 관련 컬럼 이름
+  as: 'receipts',
 });
 
 db.Items.hasMany(db.Inventory, {
-  foreignKey: "item_id", // Receipt 모델의 아이템 관련 컬럼 이름
-  as: "inventory",
+  foreignKey: 'item_id', // Receipt 모델의 아이템 관련 컬럼 이름
+  as: 'inventory',
 });
 
 // InventorySnapshots 모델에 아이템 관계 설정
 db.InventorySnapshots.belongsTo(db.Items, {
-  foreignKey: "itemId", // InventorySnapshots 모델의 컬럼 이름
-  as: "item",
+  foreignKey: 'itemId', // InventorySnapshots 모델의 컬럼 이름
+  as: 'item',
 });
 
 db.Inventory.belongsTo(db.Items, {
-  foreignKey: "item_id",
-  as: "item",
+  foreignKey: 'item_id',
+  as: 'item',
 });
 
 db.Receipt.belongsTo(db.Items, {
-  foreignKey: "item_id",
-  as: "item",
+  foreignKey: 'item_id',
+  as: 'item',
 });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 console.log(Object.keys(db));
 sequelize.sync();
-console.log("Sqeuelize sync is success");
+console.log('Sqeuelize sync is success');
 module.exports = db;
